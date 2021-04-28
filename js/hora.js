41e1019e2fe68901667598a48cbab489be0f7e29
DANIEL FELLIPE <dfellipe7@gmail.com>
2021-04-28 11:16:15 -0300

    minutos corrigidos, agora aparece decimal

--- js/hora.js
+++ js/hora.js
@@ -1,11 +1,15 @@
 // Setar Hora
+function pad(s){
+    return (s < 10) ? '0' + s : s;
+}
+
 function time(){
     let agora = new Date();
     let horas = agora.getHours();
     let minutos = agora.getMinutes();
     
     let pegarHora = document.querySelector('.hours');
-    pegarHora.innerHTML = horas + ' : ' + minutos;
+    pegarHora.innerHTML = [horas, minutos].map(pad).join(':');
     setTimeout('time()', 500);
   }
 time();
